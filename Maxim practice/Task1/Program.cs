7ce7afaa0f421f7460f83d33432e45a32acafab8
AyazKhai <ayazhhh04@gmail.com>
2023-10-31 00:45:14 +0300

    commit: task2 solution was added

--- Maxim practice/Task1/Program.cs
+++ Maxim practice/Task1/Program.cs
@@ -1,48 +1,20 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 //Выполнил Хайрутдинов Аяз https://github.com/AyazKhai/Maxim-tech-practice/blob/main/Maxim%20practice/Task1/Program.cs
 namespace Practice
 {
-    internal class Program
+    public class Task1
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Введите строку");
             string arg = Console.ReadLine();
-            Console.WriteLine(ReplacerSecOpt(arg));
+            Task2.Print(arg);
 
         }
 
         public static string Replacer(string input)
-        {
-            if (input.Length % 2 == 0)//Проверка строки на четность элементов
-            {
-                string firstHalf = input.Substring(0, input.Length / 2);//Первая половина строки
-                string SecondHalf = input.Substring(input.Length / 2);//Вторая
-
-                //создаем массивы элементов каждой полвинки и переворачиваем их
-                char[] firstHalfCharArray = firstHalf.ToCharArray();
-                Array.Reverse(firstHalfCharArray);
-
-                char[] SecondHalfCharArray = SecondHalf.ToCharArray();
-                Array.Reverse(SecondHalfCharArray);
-
-                //Объединяем перевернутые строки
-                string reversedInput = new string(firstHalfCharArray) + new string(SecondHalfCharArray);
-                return reversedInput;
-            }
-            else
-            {
-                //Переворачиваем строку
-                char[] reversefInput = input.ToCharArray();
-                Array.Reverse(reversefInput);
-
-                //Объединяем перевернутую строку с изначальной(input)
-                string reversedInput = new string(reversefInput) + input;
-                return reversedInput;
-            }
-        }
-
-        public static string ReplacerSecOpt(string input)
         {
             StringBuilder result = new StringBuilder();
             if (input.Length % 2 == 0)
